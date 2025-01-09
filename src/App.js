d2cf8cb873a88428bcd1cb5684b15b83d02d6711
rianchain <rianziwalker@gmail.com>
2025-01-09 11:10:29 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,3 +2,19 @@ import React, { useEffect, useState } from 'react'
 import Web3 from 'web3'
 import VotingContract from './Voting.json'
 
+const App = () => {
+  const [account, setAccount] = useState('')
+  const [candidates, setCandidates] = useState([])
+  const [contract, setContract] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const loadingBlockchainData = async () => {
+      const web3 = new Web3(window.ethereum)
+      const accounts = await web3.eth.requestAccounts()
+      setAccount(accounts[0])
+
+      
+    }
+  })
+}
