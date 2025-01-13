c8c4714fa9644d2f2720006e87d83bde0cc9a4db
rianchain <rianziwalker@gmail.com>
2025-01-13 13:49:47 +0700

    Add VotingContract.js: Define ABI for the Voting contract, including functions for adding candidates, voting, and retrieving candidates.

---
+++ src/abis/VotingContract.js
@@ -0,0 +1,82 @@
+export const VotingContractABI = [
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "_durationInHours",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "uint256",
+        name: "_candidateId",
+        type: "uint256",
+      },
+    ],
+    name: "votedEvent",
+    type: "event",
+  },
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "_name",
+        type: "string",
+      },
+    ],
+    name: "addCandidate",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "getCandidates",
+    outputs: [
+      {
+        components: [
+          {
+            internalType: "uint256",
+            name: "id",
+            type: "uint256",
+          },
+          {
+            internalType: "string",
+            name: "name",
+            type: "string",
+          },
+          {
+            internalType: "uint256",
+            name: "voteCount",
+            type: "uint256",
+          },
+        ],
+        internalType: "struct Voting.Candidate[]",
+        name: "",
+        type: "tuple[]",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "_candidateId",
+        type: "uint256",
+      },
+    ],
+    name: "vote",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
