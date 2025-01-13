c8a13e2d680ef20c4b3a5458c8862397155ded6f
rianchain <rianziwalker@gmail.com>
2025-01-13 23:22:03 +0700

    Refactor Login component: Update welcome message and title for application rebranding to 'VoteChain3 Pemilu'. Enhance user greeting and maintain consistency with recent UI changes.

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,9 +5,12 @@ const Login = ({ onLogin }) => {
   return (
     <div className="login-container">
       <div className="login-card">
-        <h1>Selamat Datang di :</h1>
-        <h2>Dapps Pemilu 2025</h2>
-        <p>Silakan hubungkan wallet MetaMask Anda untuk melanjutkan</p>
+        {/* <h1>Selamat Datang di :</h1> */}
+        <h2>VoteChain3 Pemilu</h2>
+        <p>
+          Selamat Datang 😉 Silakan hubungkan wallet MetaMask Anda untuk
+          melanjutkan
+        </p>
         <button className="connect-button" onClick={onLogin}>
           <img src={metamaskLogo} alt="MetaMask" className="metamask-logo" />
           <span>Hubungkan dengan MetaMask</span>
